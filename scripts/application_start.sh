14b07ec41c8f67d56af608cd1ed114b7bb5d175a
bhadanedeepraj <bhadanedeepraj@gmail.com>
2022-02-06 20:45:07 +0530

    Stop script changed again 13

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,12 @@
 #!bin/bash
+echo "Give permissions"
+sudo chmod -R 777 /home/ec2-user/express-app
 
 echo "Go into Express-app folder"
 cd /home/ec2-user/express-app
+
+echo "Give permissions"
+sudo chmod -R 777 /home/ec2-user/express-app
 
 echo "Start npm i"
 npm i
