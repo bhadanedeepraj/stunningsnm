5151cba8a6a1498594979c46b81c9d5f4851c02c
bhadanedeepraj <bhadanedeepraj@gmail.com>
2022-02-06 18:55:46 +0530

    Stop script changed again 3

---
+++ scripts/before_install.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+echo "START : install NVM command "
+yum install -y gcc-c++ make
+echo "END : install NVM command"
+
+echo "START : Download NODE 14"
+curl -sl https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+echo "END : Download NODE 14"
+
+echo "START : INSTALL NODE 14"
+yum install -y nodejs
+echo "END : INSTALL NODE 14"
+
+
+#Create Directory if not there
+DIR = "/home/ec2-user/express-app"
+if [-d "$DIR" ]; then
+    echo "${DIR} exists"
+else    
+    echo "Creating Directory ${DIR}"
+    mkdir ${DIR}
+
+
+echo "Stopping the NODE server"
+pkill node
+echo "Stopped Node Server"
